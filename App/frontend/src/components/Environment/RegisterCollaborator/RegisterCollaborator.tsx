b96051175cc3bf93047b5d0f80ba1c98bbca5894
Murilll <murilosertorio25@gmail.com>
2024-04-24 15:38:52 -0300

    create: alert

--- App/frontend/src/components/Environment/RegisterCollaborator/RegisterCollaborator.tsx
+++ App/frontend/src/components/Environment/RegisterCollaborator/RegisterCollaborator.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import './Style.scss';
+import BasicAlerts from './BasicAlerts/BasicAlerts';
 
 export default function RegisterCollaborator() {
     const router = useRouter()
@@ -16,9 +17,11 @@ export default function RegisterCollaborator() {
     const [plate, setPlate] = useState("")
     const [collaboratorId, setCollaboratorId] = useState("")
 
+    const [isActive, setActive] = useState(false)
+
     const handleClick = () => {
         router.push('/registerCollaborator');
-      };
+    };
 
     const send = async () => {
         const Collaborator = {
@@ -38,32 +41,43 @@ export default function RegisterCollaborator() {
         const res2 = await axios.post("http://localhost:5293/api/Cars", Car);
 
         handleClick()
+
+        setActive(true)
+
+        const timer = setTimeout(() => {
+            setActive(false);
+        }, 2000);
     }
 
     return (
-        <div className='Container'>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-                style={{
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
-            >
-                <TextField style={{width: '35rem'}} label="EDV" onChange={e => setEDV(e.target.value)} />
-                <TextField style={{width: '35rem'}} label="Nome do colaborador" onChange={e => setName(e.target.value)} />
-                <TextField style={{width: '35rem'}} label="Carro" onChange={e => setCar(e.target.value)} />
-                <TextField style={{width: '35rem'}} label="Cor" onChange={e => setColor(e.target.value)} />
-                <TextField style={{width: '35rem'}} label="Placa" onChange={e => setPlate(e.target.value)} />
+        <>
+            <div className='Container'>
+                <Box
+                    component="form"
+                    sx={{
+                        '& > :not(style)': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}
+                >
+                    <TextField style={{ width: '35rem' }} label="EDV" onChange={e => setEDV(e.target.value)} />
+                    <TextField style={{ width: '35rem' }} label="Nome do colaborador" onChange={e => setName(e.target.value)} />
+                    <TextField style={{ width: '35rem' }} label="Carro" onChange={e => setCar(e.target.value)} />
+                    <TextField style={{ width: '35rem' }} label="Cor" onChange={e => setColor(e.target.value)} />
+                    <TextField style={{ width: '35rem' }} label="Placa" onChange={e => setPlate(e.target.value)} />
 
-                <Button style={{width: '35rem'}} variant="contained" onClick={() => send()}>
-                    Enviar
-                </Button>
-            </Box>
-        </div>
+                    <Button style={{ width: '35rem' }} variant="contained" onClick={() => send()}>
+                        Enviar
+                    </Button>
+                </Box>
+            </div>
+            <div className="Alert">
+                {isActive && <BasicAlerts />}
+            </div>
+        </>
     )
 }
